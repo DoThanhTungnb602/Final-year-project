248a506ba769bc1c84ba92ecf9c613e23ef2be45
Đỗ Thanh Tùng <dothanhtungnb602@gmail.com>
2024-04-25 17:38:02 +0700

    resize panes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -9,27 +9,27 @@ import ConsolePanel from "~/components/playground/console-panel";
 
 export default async function Home() {
   return (
-    <main className="flex-1 overflow-auto p-3 h-full">
+    <main className="h-full flex-1 overflow-auto p-3">
       <div className="h-full">
         <ResizablePanelGroup
           direction="horizontal"
           className="h-full min-h-full w-full rounded-lg border"
         >
-          <ResizablePanel defaultSize={25} minSize={25}>
+          <ResizablePanel defaultSize={50} minSize={25}>
             <div className="h-full p-2">
               <ProblemDescriptionPanel />
             </div>
           </ResizablePanel>
           <ResizableHandle withHandle />
-          <ResizablePanel defaultSize={75} minSize={40}>
+          <ResizablePanel defaultSize={50} minSize={40}>
             <ResizablePanelGroup direction="vertical">
-              <ResizablePanel defaultSize={90} minSize={10}>
+              <ResizablePanel defaultSize={60} minSize={10}>
                 <div className="h-full p-2">
                   <EditorPanel />
                 </div>
               </ResizablePanel>
               <ResizableHandle withHandle />
-              <ResizablePanel defaultSize={10} minSize={10}>
+              <ResizablePanel defaultSize={40} minSize={10}>
                 <div className="h-full p-2">
                   <ConsolePanel />
                 </div>
