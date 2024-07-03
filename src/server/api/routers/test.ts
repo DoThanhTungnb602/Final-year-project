6c35769a9e5afae237d8e76bdbec377cc06d85b9
Đỗ Thanh Tùng <dothanhtungnb602@gmail.com>
2024-07-04 01:30:22 +0700

    fix some linting error

--- src/server/api/routers/test.ts
+++ src/server/api/routers/test.ts
@@ -36,6 +36,7 @@ export const testRouter = createTRPCRouter({
                     verdict: true,
                   },
                 },
+                tags: true,
               },
             },
           },
